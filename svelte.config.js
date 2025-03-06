30ec4ccea049e1b9b4caa950a7e2981e33b4f087
battlequestAAA <73023324+battlequestAAA@users.noreply.github.com>
2025-03-06 09:32:12 +0000

    svelte config.js change

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			}
-		})
-	}
-};
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
-};
+
+
+}
