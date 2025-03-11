4dd20bc19a63b419416bd49a9be1cd9821387b0d
battlequestAAA <73023324+battlequestAAA@users.noreply.github.com>
2025-03-11 12:45:16 +0000

    big fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 
